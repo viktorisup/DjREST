d08e1d276d6505ce61f40922a5930b6a41bbb511
Viktor-mate <Viktor.isup@mail.ru>
2023-02-10 18:00:34 +0300

    lesson_1 complite

---
+++ todo/userapp/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserSerializer
+
+
+
+class UserViewSet(ModelViewSet):
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
